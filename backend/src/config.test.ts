6715a869c52a08a91ad2fcba11ea580804346071
Mohammad Alburhan <alburhan97@gmail.com>
2024-05-24 14:03:08 +0200

    test(backend): add tests for config and is-empty util

---
+++ backend/src/config.test.ts
@@ -0,0 +1,33 @@
+import { envLoggerOptions } from "@/config";
+
+describe("envLoggerOptions", () => {
+  it("should have 'dev', 'prod', and 'test' environment logger options", () => {
+    // When, Then
+    expect(envLoggerOptions).toHaveProperty("dev");
+    expect(envLoggerOptions).toHaveProperty("prod");
+    expect(envLoggerOptions).toHaveProperty("test");
+  });
+
+  it("should have a 'dev' logger option with the correct options", () => {
+    // Given
+    const loggerOptions = envLoggerOptions.dev;
+    // Then
+    expect(loggerOptions).toMatchObject({
+      transport: {
+        target: "pino-pretty",
+        options: {
+          translateTime: "HH:MM:ss Z",
+          ignore: "pid,hostname",
+        },
+      },
+    });
+  });
+
+  it("should have a 'prod' logger option with the correct log level", () => {
+    // Given
+    const prodOption = envLoggerOptions.prod;
+    // Then
+    expect(prodOption).toBeDefined();
+    expect(prodOption?.level).toBe("info");
+  });
+});
